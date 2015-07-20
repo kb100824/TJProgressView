2437158e70a85f96470033e3a158ee66a3ec9afb
iosbsky2014 <632561914@qq.com>
2015-07-20 21:22:25 +0800

    Initial Commit

---
+++ TJProgressView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TJProgressView
+//
+//  Created by MJ on 15/7/20.
+//  Copyright (c) 2015年 TJ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ TJProgressView/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  TJProgressView
+//
+//  Created by MJ on 15/7/20.
+//  Copyright (c) 2015年 TJ. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
