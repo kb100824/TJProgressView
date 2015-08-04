76b40aa88b790b4b9f74e2209c368417a7da0a9b
ebaotong <2455063164@qq.com>
2015-08-04 10:24:18 +0800

    重新提交演示gif动画效果图

--- TJProgressView/ViewController.m
+++ TJProgressView/ViewController.m
@@ -18,7 +18,7 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
-    _progressView.progress = 0.89;
+   // _progressView.progress = 0.89;
     _progressView.progressColor = [UIColor colorWithRed:0.590 green:0.208 blue:0.041 alpha:1.000];
     // Do any additional setup after loading the view, typically from a nib.
 }
